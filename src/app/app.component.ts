033dad0809bc78e8ca80c4f8b97e04c1bbec22b7
vijaysuslade <vjsuslade@gmail.com>
2020-08-08 13:17:56 +0530

    Recative -form with validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, SimpleChanges, OnInit } from '@angular/core';
+import { Observable, interval, observable } from 'rxjs'
+import { map, filter } from 'rxjs/operators'
+import { FormBuilder, Validators, FormGroup, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'FormsPratice';
+export class AppComponent implements OnInit {
+    registerForm: FormGroup;
+    submitted = false;
+
+    constructor(private formBuilder: FormBuilder) { }
+
+    ngOnInit() {
+        this.registerForm = this.formBuilder.group({
+            title: ['', Validators.required],
+            firstName: ['', Validators.required],
+            lastName: ['', Validators.required],
+            email: ['', Validators.required],
+            password: ['', Validators.required],
+            confirmPassword:['', Validators.required],
+            acceptTerms:['', Validators.required],
+
+        });
+    }
+
+    // convenience getter for easy access to form fields
+    get f() { return this.registerForm.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.registerForm.invalid) {
+            return;
+        }
+
+        // display form values on success
+      console.log(this.registerForm.value)
+    }
+
+    onReset() {
+        this.submitted = false;
+        this.registerForm.reset();
+    }
 }
